d3b0b6ea741733d7fd51b48f4515a4ba7e51d55d
devon-wolf <devon.wolfkiel@gmail.com>
2022-08-15 15:18:40 -0700

    remove unnecessary Grid and Cell classes and tests

--- SinkAStartup.java
+++ SinkAStartup.java
@@ -1,96 +1,9 @@
-class SinkAStartup {
+class Game {
 
 }
 
-class SinkAStartupTest {
+class GameTest {
 
-}
-
-class Grid {
-	private Cell[] cells;
-
-	private void populateCells(int len) {
-		cells = new Cell[len];
-		for (int i = 0; i  < cells.length; i++) {
-			cells[i] = new Cell();
-		}
-	}
-
-	private void setGrid(String[] cols, String[] rows) {
-		populateCells(cols.length * rows.length);
-		
-		int row = 0;
-		int col = 0;
-
-		for (Cell cell:cells) {
-			cell.setCoordinates(rows[row], cols[col]);
-			row++;
-
-			if (row == rows.length) {
-				row = 0;
-				col++;
-			}
-		}
-	}
-
-	public Cell[] getGrid(String[] cols, String[] rows) {
-		setGrid(cols, rows);
-		return cells;
-	} 
-}
-
-class GridTest {
-	public static void main (String[] args) {
-		String[] cols = {"0", "1", "2", "3", "4", "5", "6"};
-		String[] rows = {"A", "B", "C", "D", "E", "F", "G"};
-		Grid grid = new Grid();
-		Cell[] mappedGrid = grid.getGrid(cols, rows);
-		System.out.println("The grid has " + mappedGrid.length + " cells");
-
-		System.out.println("Here are all the cells in the test grid:");
-		for (int i = 0; i < mappedGrid.length; i++) {
-			System.out.println(i + " Coordinates " + mappedGrid[i].getRow() + " " + mappedGrid[i].getCol());
-		}
-	}
-}
-
-class Cell {
-	private String row;
-	private String col;
-
-	private void setRow(String newRow) {
-		row = newRow;
-	}
-
-	public String getRow() {
-		return row;
-	}
-
-	private void setCol(String newCol) {
-		col = newCol;
-	}
-
-	public String getCol() {
-		return col;
-	}
-
-	public void setCoordinates(String newRow, String newCol) {
-		setRow(newRow);
-		setCol(newCol);
-	}
-
-	public String[] getCoordinates() {
-		String[] coordinates = {row, col};
-		return coordinates;
-	}
-}
-
-class CellTest {
-		public static void main (String[] args) {
-		Cell cell = new Cell();
-		cell.setCoordinates("0", "A");
-		System.out.println("Here are the test cell coordinates: " + cell.getRow() + " " + cell.getCol());
-	}
 }
 
 class Startup {
@@ -127,6 +40,7 @@ class Startup {
 					location[i] = "";
 					numOfHits++;
 					result = "Hit";
+					break;
 				}
 			}
 		}
@@ -141,7 +55,13 @@ class Startup {
 }
 
 class StartupTest extends Test {
+	private static String name = "Startup Class Tests";
+
 	public static void main(String[] args) {
+		System.out.println();
+		System.out.println("Startup Class Tests");
+		System.out.println("=====");
+
 		Startup startup = new Startup();
 		startup.setName("testup");
 
@@ -160,16 +80,16 @@ class StartupTest extends Test {
 		String miss = "Miss";
 
 		String resultHit = startup.checkGuess(guessHit1);
-		printTest("it should return a hit", resultHit.equals(hit));
+		printTest("it should return a hit for guessHit1", resultHit.equals(hit));
 
 		String resultMiss = startup.checkGuess(guessWrong);
-		printTest("it should return a miss", resultMiss.equals(miss));
+		printTest("it should return a miss for a wrong guess", resultMiss.equals(miss));
 
 		printTest("it should reflect one hit on the startup", startup.getNumOfHits() == 1);
 		printTest("it should indicate that the startup is not sunk", startup.isSunk() == false);
 
 		String hit2 = startup.checkGuess(guessHit2);
-		printTest("it should return a hit", hit2.equals(hit));
+		printTest("it should return a hit on guessHit2", hit2.equals(hit));
 
 		String dupeHit = startup.checkGuess(guessDupe);
 		printTest("it should return a miss if a previously hit cell is guessed", dupeHit.equals(miss));
@@ -180,10 +100,12 @@ class StartupTest extends Test {
 		printTest("it should still show the startup as not yet sunk", startup.isSunk() == false);
 
 		String hit3 = startup.checkGuess(guessHit3);
-		printTest("it should return a hit", hit3.equals(hit));
+		printTest("it should return a hit on guessHit3", hit3.equals(hit));
 
 		printTest("it should show the number of hits as 3", startup.getNumOfHits() == 3);
 		printTest("it should indicate that the startup is sunk after hitting all location cells", startup.isSunk() == true);
+
+		System.out.println("=====");
 	}
 }
 
@@ -202,6 +124,6 @@ class Test {
 	}
 
 	protected static void printTest(String description, boolean expectation) {
-		System.out.println(description + ": " + getPassOrFail(expectation));
+		System.out.println(getPassOrFail(expectation) + ": " + description);
 	}
 }
