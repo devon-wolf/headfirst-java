59ec21609aaf0741e398df236f9972c7f37cb5da
devon-wolf <devon.wolfkiel@gmail.com>
2022-08-23 15:57:54 -0700

    array list exercise

---
+++ ArrayListMagnet.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+public class ArrayListMagnet {
+	public static void main(String[] args) {
+		// outout should be
+		// zero one two three
+		// zero one three four
+		// zero one three four 4.2
+		// zero one three four 4.2
+
+		ArrayList<String> a = new ArrayList<>();
+		a.add(0, "zero");
+		a.add(1, "one");
+		a.add(2, "two");
+		a.add(3, "three");
+		printList(a);
+
+		a.remove(2);
+
+		if (a.contains("two")) {
+			a.add("2.2");
+		}
+		
+		if (a.contains("three")) {
+			a.add("four");
+		}
+		printList(a);
+
+		if (a.indexOf("four") != 4) {
+			a.add(4, "4.2");
+		}
+		printList(a);
+		printList(a);
+	}
+
+	public static void printList(ArrayList<String> list) {
+		for (String element : list) {
+			System.out.print(element + " ");
+		}
+		System.out.println();
+	}
+}
