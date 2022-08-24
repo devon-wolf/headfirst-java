b7fb62d5a36cde41d025901ace843ec19a881c08
devon-wolf <devon.wolfkiel@gmail.com>
2022-08-24 14:27:19 -0700

    overload duck constructor

--- Duck.java
+++ Duck.java
@@ -1,14 +1,21 @@
 public class Duck {
 	int size;
 
+	public Duck() {
+		// if no args are passed to the new Duck, set it to default size 25
+		size = 25;
+		System.out.println("Quack I am this big: " + size);
+	}
+
 	public Duck(int duckSize) {
 		size = duckSize;
-		System.out.println("Quack I am this big: " + size);	
+		System.out.println("Quack I am this big: " + size);
 	}
 }
 
 class DuckTest {
 	public static void main(String[] args) {
 		Duck d = new Duck(37);
+		Duck d2 = new Duck();
 	}
 }
